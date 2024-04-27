a1d6dfcb5e5cb792ce67b6956bac60d1300bc360
LHarveyDev <lucyanneharvey@hotmail.com>
2024-04-27 18:57:07 +0000

    add product details functionality

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
 
     def __str__(self):
@@ -15,7 +15,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
