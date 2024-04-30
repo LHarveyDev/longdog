5b47ae2459d00a51078f61d9757bc024bc6824a3
LHarveyDev <lucyanneharvey@hotmail.com>
2024-04-30 14:14:30 +0000

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn longdog.wsgi:application
--- longdog/settings.py
+++ longdog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lharveydev-longdog-acwianx8ly1.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-lharveydev-longdog-acwianx8ly1.ws-eu110.gitpod.io', 'longdog-be27da85e7e6.herokuapp.com']
 
 
 # Application definition
@@ -116,15 +117,19 @@ WSGI_APPLICATION = 'longdog.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
